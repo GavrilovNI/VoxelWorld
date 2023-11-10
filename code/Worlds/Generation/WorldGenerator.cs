2df327f1724c6d95c5186e5c1927d25a7afb515c
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-11-10 19:07:56 +0100

    feat: add player

--- code/Worlds/Generation/WorldGenerator.cs
+++ code/Worlds/Generation/WorldGenerator.cs
@@ -1,3 +1,5 @@
+﻿using Sandcube.Mth;
+using Sandcube.Worlds.Blocks;
 
 namespace Sandcube.Worlds.Generation;
 
@@ -5,5 +7,25 @@ public class WorldGenerator : BaseComponent
 {
     public void GenerateChunk(Chunk chunk)
     {
+        chunk.Clear();
+        Vector3Int blockOffset = chunk.BlockOffset;
+
+        for(int x = 0; x < chunk.Size.x; ++x)
+        {
+            for(int y = 0; y < chunk.Size.y; ++y)
+            {
+                for(int z = 0; z < chunk.Size.z; ++z)
+                {
+                    Vector3Int localBlockPosition = new(x, y, z);
+                    Vector3Int globalBlockPosition = localBlockPosition + blockOffset;
+
+                    if(globalBlockPosition.z > 10)
+                        continue;
+
+                    var block = SandcubeGame.Instance!.Blocks.Stone.DefaultBlockState;
+                    chunk.SetBlockState(localBlockPosition, block);
+                }
+            }
+        }
     }
 }
