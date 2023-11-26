8f80244a5afd148b517c864af0de65575df056fc
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-11-26 19:30:06 +0100

    feat: add BBox.Expanded

---
+++ code/Mth/BBoxExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandcube.Mth;
+
+public static class BBoxExtensions
+{
+    public static BBox Expanded(this BBox bbox, Vector3 value)
+    {
+        var result = bbox;
+        result.Mins -= value;
+        result.Maxs += value;
+        return result;
+    }
+}
