ef4e20a7f0d3332bf02bf349f50853504c2ecac5
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-01 23:59:37 +0100

    feat: add SandcubeGame.Instance!.BlocksTextureMap shortcut property

--- code/Blocks/SandcubeBlocks.cs
+++ code/Blocks/SandcubeBlocks.cs
@@ -8,11 +8,14 @@ namespace Sandcube.Blocks;
 
 public sealed class SandcubeBlocks : ModRegisterables<Block>
 {
+    private static PathedTextureMap TextureMap => SandcubeGame.Instance!.BlocksTextureMap;
+
     private static string GetBlockPathPart(string blockId) => $"{SandcubeBaseMod.ModName}/blocks/{blockId}";
 
     private static ModedId MakeId(string blockId) => new(SandcubeBaseMod.ModName, blockId);
 
-    public SimpleBlock Air { get; } = new(MakeId("air"), SandcubeGame.Instance!.BlocksTextureMap.Transparent)
+
+    public SimpleBlock Air { get; } = new(MakeId("air"), TextureMap.Transparent)
     {
         Properties = BlockProperties.Default with
         {
@@ -20,11 +23,11 @@ public sealed class SandcubeBlocks : ModRegisterables<Block>
         }
     };
 
-    public SimpleBlock Stone { get; } = new(MakeId("stone"), SandcubeGame.Instance!.BlocksTextureMap.GetOrLoadTexture($"{GetBlockPathPart("stone")}.png"));
-    public SlabBlock StoneSlab { get; } = new(MakeId("stone_slab"), SandcubeGame.Instance!.BlocksTextureMap.GetOrLoadTexture($"{GetBlockPathPart("stone")}.png"));
-    public SimpleBlock Dirt { get; } = new(MakeId("dirt"), SandcubeGame.Instance!.BlocksTextureMap.GetOrLoadTexture($"{GetBlockPathPart("dirt")}.png"));
-    public SimpleBlock Cobblestone { get; } = new(MakeId("cobblestone"), SandcubeGame.Instance!.BlocksTextureMap.GetOrLoadTexture($"{GetBlockPathPart("cobblestone")}.png"));
-    public SimpleBlock Glass { get; } = new(MakeId("glass"), SandcubeGame.Instance!.BlocksTextureMap.GetOrLoadTexture($"{GetBlockPathPart("glass")}.png"))
+    public SimpleBlock Stone { get; } = new(MakeId("stone"), TextureMap.GetOrLoadTexture($"{GetBlockPathPart("stone")}.png"));
+    public SlabBlock StoneSlab { get; } = new(MakeId("stone_slab"), TextureMap.GetOrLoadTexture($"{GetBlockPathPart("stone")}.png"));
+    public SimpleBlock Dirt { get; } = new(MakeId("dirt"), TextureMap.GetOrLoadTexture($"{GetBlockPathPart("dirt")}.png"));
+    public SimpleBlock Cobblestone { get; } = new(MakeId("cobblestone"), TextureMap.GetOrLoadTexture($"{GetBlockPathPart("cobblestone")}.png"));
+    public SimpleBlock Glass { get; } = new(MakeId("glass"), TextureMap.GetOrLoadTexture($"{GetBlockPathPart("glass")}.png"))
     {
         Properties = BlockProperties.Default with
         {
@@ -33,9 +36,9 @@ public sealed class SandcubeBlocks : ModRegisterables<Block>
     };
 
     public PillarBlock WoodLog { get; } = new(MakeId("wood_log"),
-        BlockTexturesLoader.SimplePillar.LoadTextureUvs(SandcubeGame.Instance!.BlocksTextureMap, GetBlockPathPart("wood_log")));
+        BlockTexturesLoader.SimplePillar.LoadTextureUvs(TextureMap, GetBlockPathPart("wood_log")));
 
     public HorizontalDirectionalBlock Furnace { get; } = new(MakeId("furnace"),
         BlockTexturesLoader.SimplePillar.With(Direction.Forward, BlockTexturesLoader.FrontSuffix)
-        .LoadTextureUvs(SandcubeGame.Instance!.BlocksTextureMap, GetBlockPathPart("furnace")));
+        .LoadTextureUvs(TextureMap, GetBlockPathPart("furnace")));
 }
