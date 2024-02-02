28b3ce9ecfa946a97e973de0e1a481bf8a45db99
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-02 01:28:24 +0100

    feat: update Vector3Int

--- code/Mth/Vector3Int.cs
+++ code/Mth/Vector3Int.cs
@@ -1,13 +1,15 @@
 ﻿using Sandbox;
 using Sandcube.Mth.Enums;
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 
 namespace Sandcube.Mth;
 
-public struct Vector3Int
+public struct Vector3Int : IEquatable<Vector3Int>, IParsable<Vector3Int>
 {
     public static readonly Vector3Int One = new(1);
     public static readonly Vector3Int Zero = new(0);
@@ -58,6 +60,7 @@ public struct Vector3Int
         }
     }
 
+    [JsonIgnore]
     public readonly float Length => MathF.Sqrt(LengthSquared);
 
     [JsonIgnore]
@@ -79,30 +82,28 @@ public struct Vector3Int
         new(Math.Clamp(x, otherMin.x, otherMax.x), Math.Clamp(y, otherMin.y, otherMax.y), Math.Clamp(z, otherMin.z, otherMax.z));
     public readonly Vector3 Clamp(Vector3 otherMin, Vector3 otherMax) =>
         new(Math.Clamp(x, otherMin.x, otherMax.x), Math.Clamp(y, otherMin.y, otherMax.y), Math.Clamp(z, otherMin.z, otherMax.z));
-    public readonly Vector3Int Clamp(int otherMin, int otherMax) => Clamp((Vector3Int)otherMin, (Vector3Int)otherMax);
-    public readonly Vector3 Clamp(float otherMin, float otherMax) => Clamp((Vector3)otherMin, (Vector3)otherMax);
+    public readonly Vector3Int Clamp(int otherMin, int otherMax) =>
+        new(Math.Clamp(x, otherMin, otherMax), Math.Clamp(y, otherMin, otherMax), Math.Clamp(z, otherMin, otherMax));
+    public readonly Vector3 Clamp(float otherMin, float otherMax) =>
+        new(Math.Clamp(x, otherMin, otherMax), Math.Clamp(y, otherMin, otherMax), Math.Clamp(z, otherMin, otherMax));
 
     public readonly Vector3Int ComponentMin(Vector3Int other) => new(Math.Min(x, other.x), Math.Min(y, other.y), Math.Min(z, other.z));
     public readonly Vector3 ComponentMin(Vector3 other) => new(Math.Min(x, other.x), Math.Min(y, other.y), Math.Min(z, other.z));
     public static Vector3Int Min(Vector3Int a, Vector3Int b) => a.ComponentMin(b);
-    public static Vector3 Min(Vector3 a, Vector3 b) => a.ComponentMin(b);
 
     public readonly Vector3Int ComponentMax(Vector3Int other) => new(Math.Max(x, other.x), Math.Max(y, other.y), Math.Max(z, other.z));
     public readonly Vector3 ComponentMax(Vector3 other) => new(Math.Max(x, other.x), Math.Max(y, other.y), Math.Max(z, other.z));
     public static Vector3Int Max(Vector3Int a, Vector3Int b) => a.ComponentMax(b);
-    public static Vector3 Max(Vector3 a, Vector3 b) => a.ComponentMax(b);
 
     public readonly Vector3 LerpTo(Vector3 target, float frac, bool clamp = true) => Vector3.Lerp(this, target, frac, clamp);
 
     public static Vector3Int Cross(Vector3Int a, Vector3Int b) => new(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
-    public static Vector3 Cross(Vector3 a, Vector3 b) => new(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
     public readonly Vector3Int Cross(Vector3Int b) => Cross(this, b);
-    public readonly Vector3 Cross(Vector3 b) => Cross(this, b);
+    public readonly Vector3 Cross(Vector3 b) => Vector3.Cross(this, b);
 
     public static int Dot(Vector3Int a, Vector3Int b) => a.x * b.x + a.y * b.y + a.z * b.z;
-    public static float Dot(Vector3 a, Vector3 b) => a.x * b.x + a.y * b.y + a.z * b.z;
     public readonly int Dot(Vector3Int b) => Dot(this, b);
-    public readonly float Dot(Vector3 b) => Dot(this, b);
+    public readonly float Dot(Vector3 b) => Vector3.Dot(this, b);
 
     public static float DistanceBetween(Vector3Int a, Vector3 b) => (b - a).Length;
     public static float DistanceBetween(Vector3 a, Vector3Int b) => (b - a).Length;
@@ -116,13 +117,11 @@ public struct Vector3Int
 
     public readonly Vector3Int Abs() => new(Math.Abs(x), Math.Abs(y), Math.Abs(z));
 
-    public static Vector3 Reflect(Vector3 direction, Vector3 normal) => direction - 2f * Dot(direction, normal) * normal;
     public static Vector3Int Reflect(Vector3Int direction, Vector3Int normal) => direction - 2 * Dot(direction, normal) * normal;
 
     public static Vector3Int VectorPlaneProject(Vector3Int v, Vector3Int planeNormal) => v - v.ProjectOnNormal(planeNormal);
-    public static Vector3 VectorPlaneProject(Vector3 v, Vector3 planeNormal) => v - v.ProjectOnNormal(planeNormal);
     public readonly Vector3Int ProjectOnNormal(Vector3Int normal) => normal * Dot(this, normal);
-    public readonly Vector3 ProjectOnNormal(Vector3 normal) => normal * Dot(this, normal);
+    public readonly Vector3 ProjectOnNormal(Vector3 normal) => normal * Vector3.Dot(this, normal);
 
     public static void Sort(ref Vector3Int min, ref Vector3Int max)
     {
@@ -176,6 +175,9 @@ public struct Vector3Int
 
     public readonly Vector3 RotateAround(Vector3 center, Rotation rot) => ((Vector3)this).RotateAround(center, rot);
 
+    public readonly Vector3 WithAcceleration(Vector3 target, float acceleration) => ((Vector3)this).WithAcceleration(target, acceleration);
+    public readonly Vector3 WithFriction(float frictionAmount, float stopSpeed = 140f) => ((Vector3)this).WithFriction(frictionAmount, stopSpeed);
+
     public readonly int GetAxis(Axis axis)
     {
         if(axis == Axis.X)
@@ -211,7 +213,7 @@ public struct Vector3Int
 
     public readonly Vector3Int WithAxes(Func<int, int> axisChanger) => WithAxes((_, v) => axisChanger.Invoke(v));
 
-    public bool IsAnyAxis(Func<Axis, int, bool> predicate)
+    public readonly bool IsAnyAxis(Func<Axis, int, bool> predicate)
     {
         foreach(var axis in Axis.All)
         {
@@ -220,9 +222,9 @@ public struct Vector3Int
         }
         return false;
     }
-    public bool IsAnyAxis(Func<int, bool> predicate) => IsAnyAxis((a, v) => predicate.Invoke(v));
+    public readonly bool IsAnyAxis(Func<int, bool> predicate) => IsAnyAxis((a, v) => predicate.Invoke(v));
 
-    public bool IsEveryAxis(Func<Axis, int, bool> predicate)
+    public readonly bool IsEveryAxis(Func<Axis, int, bool> predicate)
     {
         foreach(var axis in Axis.All)
         {
@@ -231,14 +233,14 @@ public struct Vector3Int
         }
         return true;
     }
-    public bool IsEveryAxis(Func<int, bool> predicate) => IsAnyAxis((a, v) => predicate.Invoke(v));
+    public readonly bool IsEveryAxis(Func<int, bool> predicate) => IsAnyAxis((a, v) => predicate.Invoke(v));
 
-    public void EachAxis(Action<Axis, int> action)
+    public readonly void EachAxis(Action<Axis, int> action)
     {
         foreach(var axis in Axis.All)
             action.Invoke(axis, GetAxis(axis));
     }
-    public void EachAxis(Action<int> action) => EachAxis((a, v) => action.Invoke(v));
+    public readonly void EachAxis(Action<int> action) => EachAxis((a, v) => action.Invoke(v));
 
 
     public static Vector3Int operator +(Vector3Int a, Vector3Int b) => new(a.x + b.x, a.y + b.y, a.z + b.z);
@@ -261,8 +263,8 @@ public struct Vector3Int
 
     public static implicit operator Vector3(Vector3Int vector) => new(vector.x, vector.y, vector.z);
     public static implicit operator System.Numerics.Vector3(Vector3Int vector) => new(vector.x, vector.y, vector.z);
-    public static explicit operator Vector3Int(Vector3 vector) => new((int)vector.x, (int)vector.y, (int)vector.z);
     public static explicit operator Vector3Int(Vector2 vector) => new((int)vector.x, (int)vector.y, 0);
+    public static explicit operator Vector3Int(Vector3 vector) => new((int)vector.x, (int)vector.y, (int)vector.z);
     public static explicit operator Vector3Int(Vector4 vector) => new((int)vector.x, (int)vector.y, (int)vector.z);
     public static explicit operator Vector3Int(Color color) => (Vector3Int)(Vector3)color;
     public static explicit operator Vector3Int(System.Numerics.Vector3 vector) => new((int)vector.X, (int)vector.Y, (int)vector.Z);
@@ -277,13 +279,52 @@ public struct Vector3Int
             return Equals(vector);
         return false;
     }
-
-    public readonly bool Equals(Vector3 vector)
-    {
-        return this == vector;
-    }
+    public readonly bool Equals(Vector3Int vector) => this == vector;
 
     public override readonly int GetHashCode() => HashCode.Combine(x, y, z);
+
+
+    public static Vector3Int Parse(string str, IFormatProvider? _ = null)
+    {
+        if(TryParse(str, CultureInfo.InvariantCulture, out var result))
+            return result;
+
+        throw new FormatException($"couldn't parse {str} to {nameof(Vector3Int)}");
+    }
+
+    //
+    // Summary:
+    //     Given a string, try to convert this into a vector. Example input formats that
+    //     work would be "1,1,1", "1;1;1", "[1 1 1]". This handles a bunch of different
+    //     separators ( ' ', ',', ';', '\n', '\r' ). It also trims surrounding characters
+    //     ('[', ']', ' ', '\n', '\r', '\t', '"').
+    public static bool TryParse([NotNullWhen(true)] string? str, IFormatProvider? provider, [MaybeNullWhen(false)] out Vector3Int result)
+    {
+        result = Zero;
+        if(string.IsNullOrWhiteSpace(str))
+        {
+            return false;
+        }
+
+        str = str.Trim('[', ']', ' ', '\n', '\r', '\t', '"');
+        string[] array = str.Split(new char[5] { ' ', ',', ';', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        if(array.Length != 3)
+        {
+            return false;
+        }
+
+        if(!int.TryParse(array[0], NumberStyles.Integer, provider, out var result2) ||
+            !int.TryParse(array[1], NumberStyles.Integer, provider, out var result3) ||
+            !int.TryParse(array[2], NumberStyles.Integer, provider, out var result4))
+        {
+            return false;
+        }
+
+        result = new Vector3Int(result2, result3, result4);
+        return true;
+    }
+
+    public static bool TryParse(string? str, out Vector3Int result) => TryParse(str, CultureInfo.InvariantCulture, out result);
 
 
     public override readonly string ToString() => ToString("0.####");
