600c23fadcafa188883f3a80eec49a71119c5e63
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-03 01:01:52 +0100

    feat: add ability to move stacks in player inventory ui

---
+++ code/UI/Inventories/ISlotMouseEventListener.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using Sandcube.Inventories;
+using Sandcube.Items;
+
+namespace Sandcube.UI.Inventories;
+
+public interface ISlotMouseEventListener
+{
+    void OnMouseClickedOnSlot(IIndexedCapability<Stack<Item>> capability, int slotIndex, MouseButtons mouseButton) { }
+    void OnMouseDownOnSlot(IIndexedCapability<Stack<Item>> capability, int slotIndex, MouseButtons mouseButton) { }
+    void OnMouseUpOnSlot(IIndexedCapability<Stack<Item>> capability, int slotIndex, MouseButtons mouseButton) { }
+}
