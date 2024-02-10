46dc4216376916bba76fb5f0f987210590e429d1
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-10 15:54:10 +0100

    feat: add BlocksTextureMapShower

---
+++ code/Texturing/BlocksTextureMapShower.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox;
+
+namespace Sandcube.Texturing;
+
+public class BlocksTextureMapShower : Component
+{
+    [Property]
+    public ModelRenderer ModelRenderer { get; set; } = null!;
+
+    protected override void OnUpdate()
+    {
+        var texture = SandcubeGame.Instance?.BlocksTextureMap.Texture;
+        if(texture is not null)
+        {
+            ModelRenderer.SceneObject.Attributes.Set("color", texture);
+
+            var gameobject = ModelRenderer.GameObject;
+            var scale = gameobject.Transform.Scale;
+            gameobject.Transform.Scale = scale.WithY(scale.x * texture.Size.y / texture.Size.x);
+        }
+    }
+}
