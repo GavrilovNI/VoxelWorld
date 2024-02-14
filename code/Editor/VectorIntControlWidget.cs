61c2d30d541d813b6c718efeb0d688dedc25b212
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-14 17:16:51 +0100

    feat: add CustomEditor for Vector2Int

---
+++ code/Editor/VectorIntControlWidget.cs
@@ -0,0 +1,15 @@
+﻿using Editor;
+using Sandbox;
+using Sandcube.Mth;
+
+namespace Sandcube.Editor;
+
+[CustomEditor(typeof(Vector3Int))]
+[CustomEditor(typeof(Vector2Int))]
+public class VectorIntControlWidget : VectorControlWidget
+{
+    public VectorIntControlWidget(SerializedProperty property) : base(property)
+    {
+
+    }
+}
