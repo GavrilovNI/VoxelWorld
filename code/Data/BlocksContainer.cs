38788698878ac5b6a6c860230fe7701f3e8c9355
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-21 20:17:58 +0100

    feat: add IReadOnlyBlocksContainer

--- code/Data/BlocksContainer.cs
+++ code/Data/BlocksContainer.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Sandcube.Data;
 
-public class BlocksContainer
+public class BlocksContainer : IReadOnlyBlocksContainer
 {
     public Dictionary<Vector3Int, BlockState> BlockStates = new();
     public SortedDictionary<Vector3Int, BlockEntity> BlockEntities = new(Vector3Int.XYZIterationComparer);
@@ -15,6 +15,9 @@ public class BlocksContainer
         BlockStates.Clear();
         BlockEntities.Clear();
     }
+
+    public BlockState? GetBlockState(Vector3Int position) => BlockStates!.GetValueOrDefault(position, null);
+    public BlockEntity? GetBlockEntity(Vector3Int position) => BlockEntities!.GetValueOrDefault(position, null);
 
     public bool IsEmpty() => BlockStates.Count == 0 && BlockEntities.Count == 0;
 }
