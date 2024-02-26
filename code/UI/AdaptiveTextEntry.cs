daa7422b0f346c7968fd2c3091cf54248a5ccf16
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-26 01:12:45 +0100

    feat: add ui for game loading & creating

---
+++ code/UI/AdaptiveTextEntry.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox.UI;
+
+namespace Sandcube.UI;
+
+public class AdaptiveTextEntry : TextEntry
+{
+    public AdaptiveTextEntry()
+    {
+        Label.Parent = null;
+        Label = AddChild<AdaptiveLabel>("content-label");
+    }
+}
---
+++ code/UI/Menus/MainMenus/MainMenuController.cs
@@ -0,0 +1,51 @@
+﻿using Sandbox;
+using System;
+
+namespace Sandcube.UI.Menus.MainMenus;
+
+public class MainMenuController : Component
+{
+    [Property] protected MainMenu MainMenu { get; set; } = null!;
+
+    protected PanelComponent CurrentMenu { get; set; } = null!;
+
+    protected override void OnEnabled()
+    {
+        var menus = Components.GetAll<PanelComponent>();
+        foreach(var menu in menus)
+        {
+            if(menu == MainMenu)
+                continue;
+            menu.Enabled = false;
+        }
+        OpenMenu(MainMenu);
+    }
+
+    public void OpenMenu(PanelComponent menu)
+    {
+        ArgumentNullException.ThrowIfNull(menu);
+
+        if(CurrentMenu is not null)
+        {
+            if(CurrentMenu == menu)
+                return;
+            CurrentMenu.Enabled = false;
+        }
+        CurrentMenu = menu;
+        CurrentMenu.Enabled = true;
+    }
+
+    public void CloseMenu(PanelComponent menu)
+    {
+        ArgumentNullException.ThrowIfNull(menu);
+
+        if(CurrentMenu == MainMenu)
+            return;
+        if(CurrentMenu != menu)
+            return;
+
+        OpenMenu(MainMenu);
+    }
+
+    public void CloseCurrentMenu() => CloseMenu(CurrentMenu);
+}
