e02334da5edb3f9b9f6db7975daa6ce8cab9c941
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-02-28 16:54:47 +0100

    feat: add Id.TryFromString & Id.TryFromCamelCase

--- code/Registries/Id.cs
+++ code/Registries/Id.cs
@@ -1,22 +1,53 @@
 ﻿using Sandcube.IO;
 using System;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Sandcube;
 
 public readonly record struct Id : IBinaryWritable, IBinaryStaticReadable<Id>
 {
+    private const char Underscore = '_';
     public static readonly Regex Regex = new("^[a-z_]+$");
 
     public readonly string Name { get; init; }
 
     public Id(string name)
     {
-        if(string.IsNullOrEmpty(name) || !Regex.IsMatch(name))
+        if(!IsValidString(name))
             throw new ArgumentException($"{nameof(name)} should match {nameof(Regex)}");
         Name = name;
     }
+
+    public static bool TryFromString(string name, out Id id)
+    {
+        bool canConvert = IsValidString(name);
+        id = canConvert ? new(name) : default;
+        return canConvert;
+    }
+
+    public static bool TryFromCamelCase(string value, out Id id)
+    {
+        StringBuilder builder = new();
+
+        for(int i = 0; i < value.Length; ++i)
+        {
+            char c = value[i];
+
+            if(Char.IsUpper(c))
+            {
+                c = Char.ToLower(c);
+                if(i != 0 && value[i - 1] != '_')
+                    builder.Append(Underscore);
+            }
+            builder.Append(c);
+        }
+
+        return TryFromString(builder.ToString(), out id);
+    }
+
+    public static bool IsValidString(string name) => !string.IsNullOrEmpty(name) && Regex.IsMatch(name);
 
     public static implicit operator string(Id id) => id.ToString();
     public static explicit operator Id(string id) => new(id);
