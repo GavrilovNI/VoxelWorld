e2e573dce8d409e3e253f1b05a8a22658b71ae66
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-02 00:14:33 +0100

    feat: add ChunkLoaded & ChunkUnloaded to IWorldProvider

--- code/Worlds/IWorldProvider.cs
+++ code/Worlds/IWorldProvider.cs
@@ -1,9 +1,13 @@
 ﻿using Sandcube.Mth;
+using System;
 
 namespace Sandcube.Worlds;
 
 public interface IWorldProvider : IBlockStateProvider, IBlockEntityProvider, IWorldBlockPositioner
 {
+    event Action<Vector3Int>? ChunkLoaded;
+    event Action<Vector3Int>? ChunkUnloaded;
+
     BBoxInt Limits { get; }
 
     bool HasChunk(Vector3Int chunkPosition);
