2915e97cbbaf8ee6a9b323f3a0152556bc8f1b51
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-02 05:04:11 +0100

    feat: add GameInfo.Seed

--- code/Data/GameInfo.cs
+++ code/Data/GameInfo.cs
@@ -6,21 +6,25 @@ namespace Sandcube.Data;
 public readonly record struct GameInfo : IBinaryWritable, IBinaryStaticReadable<GameInfo>
 {
     public required string Name { get; init; }
+    public required int Seed { get; init; }
 
 
 
     public static GameInfo Read(BinaryReader reader)
     {
         var name = reader.ReadString();
+        var seed = reader.ReadInt32();
 
         return new GameInfo()
         {
             Name = name,
+            Seed = seed,
         };
     }
 
     public readonly void Write(BinaryWriter writer)
     {
         writer.Write(Name);
+        writer.Write(Seed);
     }
 }
