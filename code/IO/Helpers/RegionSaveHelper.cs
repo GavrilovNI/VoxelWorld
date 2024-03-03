aef5ab5189915651f1cd90752f4881747d4f4b68
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-03 19:18:43 +0100

    feat: add RegionalChunkedSaveHelper

--- code/IO/Helpers/RegionSaveHelper.cs
+++ code/IO/Helpers/RegionSaveHelper.cs
@@ -1,84 +1,42 @@
 ﻿using Sandbox;
 using Sandcube.Blocks.States;
 using Sandcube.Data;
-using Sandcube.Data.Enumarating;
 using Sandcube.Mth;
-using System;
 using System.IO;
 using System.Collections.Generic;
 
 namespace Sandcube.IO.Helpers;
 
-public class RegionSaveHelper : IBinaryWritable, IBinaryReadable
+public class RegionSaveHelper : RegionalChunkedSaveHelper<BlocksData>
 {
     protected readonly WorldOptions WorldOptions;
 
     protected BlockStatePalette BlockStatePalette;
-    protected readonly int MaxChunksCount;
-    protected readonly BBoxInt Bounds;
-
-    protected readonly Dictionary<Vector3Int, BlocksData> Chunks = new();
 
     protected virtual IEnumerable<Vector3Int> AllBlockPositionsInChunk => WorldOptions.ChunkSize.GetPositionsFromZero(false);
-
-    public RegionSaveHelper(in WorldOptions worldOptions)
+    
+    public RegionSaveHelper(in WorldOptions worldOptions) : base(worldOptions.RegionSize)
     {
         WorldOptions = worldOptions;
-
-        var regionSize = WorldOptions.RegionSize;
-        MaxChunksCount = regionSize.x * regionSize.y * regionSize.z;
-        Bounds = BBoxInt.FromMinsAndSize(0, regionSize);
-
         BlockStatePalette = new BlockStatePalette();
         UpdateBlockStatePalette();
     }
 
-    public virtual void Clear()
+    public override void Clear()
     {
-        Chunks.Clear();
+        base.Clear();
         UpdateBlockStatePalette();
     }
 
-    public virtual void SetChunkData(Vector3Int localChunkPosition, BlocksData blocksData)
-    {
-        if(!Bounds.Contains(localChunkPosition))
-            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
-
-        Chunks[localChunkPosition] = blocksData;
-    }
-
-    public virtual bool RemoveChunksData(Vector3Int localChunkPosition) =>
-        Chunks.Remove(localChunkPosition);
-
-    public virtual BlocksData? GetChunkData(Vector3Int localChunkPosition)
-    {
-        if(!Bounds.Contains(localChunkPosition))
-            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
-
-        return Chunks!.GetValueOrDefault(localChunkPosition, null);
-    }
-
-    public virtual void Write(BinaryWriter writer)
+    protected override void WriteAdditionalData(BinaryWriter writer)
     {
         UpdateBlockStatePalette();
         writer.Write(BlockStatePalette);
-        WriteChunks(writer);
     }
 
-    void IBinaryReadable.Read(BinaryReader reader) => Read(reader, true);
-    public virtual void Read(BinaryReader reader, bool readToEnd = true)
+    protected override void ReadAdditionalData(BinaryReader reader)
     {
         BlockStatePalette = BlockStatePalette.Read(reader);
-        ReadChunks(reader, readToEnd);
-    }
-
-    public virtual bool ReadOnlyOneChunk(BinaryReader reader, Vector3Int localChunkPosition, bool readToEnd = true)
-    {
-        if(!Bounds.Contains(localChunkPosition))
-            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
-
-        BlockStatePalette = BlockStatePalette.Read(reader);
-        return ReadChunk(reader, localChunkPosition, readToEnd);
     }
 
     protected virtual void UpdateBlockStatePalette()
@@ -95,57 +53,7 @@ public class RegionSaveHelper : IBinaryWritable, IBinaryReadable
         }
     }
 
-    #region read
-
-    protected virtual void ReadChunks(BinaryReader reader, bool readToEnd = true)
-    {
-        long[] chunkOffsets = ReadChunkOffsets(reader);
-        long chunksEnd = reader.ReadInt64();
-
-        var chunksStart = reader.BaseStream.Position;
-        for(int i = 0; i < MaxChunksCount; ++i)
-        {
-            var chunkOffset = chunkOffsets[i];
-            if(chunkOffset < 0)
-                continue;
-
-            reader.BaseStream.Position = chunksStart + chunkOffset;
-            Vector3Int chunkPosition = GetChunkPosition(i);
-            Chunks[chunkPosition] = ReadChunkData(reader);
-        }
-
-        if(readToEnd)
-            reader.BaseStream.Position = chunksEnd;
-    }
-
-    protected virtual bool ReadChunk(BinaryReader reader, Vector3Int chunkPosition, bool readToEnd = true)
-    {
-        long[] chunkOffsets = ReadChunkOffsets(reader);
-        long chunksEnd = reader.ReadInt64();
-
-        int index = GetChunkIndex(chunkPosition);
-        var chunkOffset = chunkOffsets[index];
-        if(chunkOffset >= 0)
-        {
-            reader.BaseStream.Position += chunkOffset;
-            Chunks[chunkPosition] = ReadChunkData(reader);
-        }
-
-        if(readToEnd)
-            reader.BaseStream.Position = chunksEnd;
-
-        return true;
-    }
-
-    protected virtual long[] ReadChunkOffsets(BinaryReader reader)
-    {
-        long[] chunkOffsets = new long[MaxChunksCount];
-        for(int i = 0; i < MaxChunksCount; ++i)
-            chunkOffsets[i] = reader.ReadInt64();
-        return chunkOffsets;
-    }
-
-    protected virtual BlocksData ReadChunkData(BinaryReader reader)
+    protected override BlocksData ReadChunkData(BinaryReader reader)
     {
         var data = new BlocksData();
 
@@ -171,52 +79,17 @@ public class RegionSaveHelper : IBinaryWritable, IBinaryReadable
         return data;
     }
 
-    #endregion
-
-    #region write
-
-    protected virtual void WriteChunks(BinaryWriter writer)
+    protected override void WriteChunkData(BinaryWriter writer, BlocksData data)
     {
-        long chunksOffsetsStart = writer.BaseStream.Position;
-
-        for(int i = 0; i < MaxChunksCount; ++i)
-            writer.Write(-1L);
-        writer.Write(-1L); // writing end chunks offset
-
-        long chunksStart = writer.BaseStream.Position;
-
-        foreach(var (chunkPosition, chunk) in Chunks)
-        {
-            using(StreamPositionRememberer rememberer = writer)
-            {
-                int chunkIndex = GetChunkIndex(chunkPosition);
-                long chunkOffset = rememberer.StartPosition - chunksStart;
-                writer.BaseStream.Position = chunksOffsetsStart + chunkIndex * sizeof(long);
-                writer.Write(chunkOffset);
-            }
-            WriteChunkData(writer, chunkPosition);
-        }
-
-        using(StreamPositionRememberer rememberer = writer)
-        {
-            writer.BaseStream.Position = chunksOffsetsStart + MaxChunksCount * sizeof(long);
-            writer.Write(rememberer.StartPosition);
-        }
-    }
-
-    protected virtual void WriteChunkData(BinaryWriter writer, Vector3Int chunkPosition)
-    {
-        var chunk = Chunks[chunkPosition];
-
         foreach(var blockPosition in AllBlockPositionsInChunk)
         {
-            var blockState = chunk.BlockStates.GetValueOrDefault(blockPosition, BlockState.Air);
+            var blockState = data.BlockStates.GetValueOrDefault(blockPosition, BlockState.Air);
             var blockStateId = BlockStatePalette!.GetId(blockState);
             writer.Write(blockStateId);
         }
 
-        writer.Write(chunk.BlockEntitiesData.Count);
-        foreach(var (blockPosition, blockEntityData) in chunk.BlockEntitiesData)
+        writer.Write(data.BlockEntitiesData.Count);
+        foreach(var (blockPosition, blockEntityData) in data.BlockEntitiesData)
         {
             if(blockEntityData.Length > 0)
             {
@@ -225,25 +98,5 @@ public class RegionSaveHelper : IBinaryWritable, IBinaryReadable
                 writer.Write(blockEntityData);
             }
         }
-    }
-
-    #endregion
-
-
-    protected virtual int GetChunkIndex(Vector3Int chunkPosition)
-    {
-        var regionSize = WorldOptions.RegionSize;
-        return chunkPosition.z + regionSize.z * (chunkPosition.y + chunkPosition.x * regionSize.y);
-    }
-
-    protected virtual Vector3Int GetChunkPosition(int chunkIndex)
-    {
-        var regionSize = WorldOptions.RegionSize;
-        int z = chunkIndex % regionSize.z;
-        chunkIndex /= regionSize.z;
-        int y = chunkIndex % regionSize.y;
-        chunkIndex /= regionSize.y;
-        int x = chunkIndex % regionSize.x;
-        return new(x, y, z);
     }
 }
---
+++ code/IO/Helpers/RegionalChunkedSaveHelper.cs
@@ -0,0 +1,179 @@
+﻿using Sandcube.Mth;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sandcube.IO.Helpers;
+
+public abstract class RegionalChunkedSaveHelper<T> : IBinaryWritable, IBinaryReadable where T : class
+{
+    public Vector3Int RegionSize { get; }
+
+    protected readonly int MaxChunksCount;
+    protected readonly BBoxInt Bounds;
+    protected readonly Dictionary<Vector3Int, T> Chunks = new();
+
+    public RegionalChunkedSaveHelper(Vector3Int regionSize)
+    {
+        RegionSize = regionSize;
+        MaxChunksCount = RegionSize.x * RegionSize.y * RegionSize.z;
+        Bounds = BBoxInt.FromMinsAndSize(0, RegionSize);
+    }
+
+
+    public virtual void Clear()
+    {
+        Chunks.Clear();
+    }
+
+    public virtual void SetChunkData(Vector3Int localChunkPosition, T blocksData)
+    {
+        if(!Bounds.Contains(localChunkPosition))
+            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
+
+        Chunks[localChunkPosition] = blocksData;
+    }
+
+    public virtual bool RemoveChunkData(Vector3Int localChunkPosition) =>
+        Chunks.Remove(localChunkPosition);
+
+    public virtual T? GetChunkData(Vector3Int localChunkPosition)
+    {
+        if(!Bounds.Contains(localChunkPosition))
+            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
+
+        return Chunks!.GetValueOrDefault(localChunkPosition, null);
+    }
+
+
+    #region read
+
+    protected virtual void ReadAdditionalData(BinaryReader reader) { }
+
+    void IBinaryReadable.Read(BinaryReader reader) => Read(reader, true);
+    public virtual void Read(BinaryReader reader, bool readToEnd = true)
+    {
+        ReadAdditionalData(reader);
+        ReadChunks(reader, readToEnd);
+    }
+
+    public virtual bool ReadOnlyOneChunk(BinaryReader reader, Vector3Int localChunkPosition, bool readToEnd = true)
+    {
+        if(!Bounds.Contains(localChunkPosition))
+            throw new ArgumentOutOfRangeException(nameof(localChunkPosition));
+
+        ReadAdditionalData(reader);
+        return ReadChunk(reader, localChunkPosition, readToEnd);
+    }
+
+    protected virtual void ReadChunks(BinaryReader reader, bool readToEnd = true)
+    {
+        long[] chunkOffsets = ReadChunkOffsets(reader);
+        long chunksEnd = reader.ReadInt64();
+
+        var chunksStart = reader.BaseStream.Position;
+        for(int i = 0; i < MaxChunksCount; ++i)
+        {
+            var chunkOffset = chunkOffsets[i];
+            if(chunkOffset < 0)
+                continue;
+
+            reader.BaseStream.Position = chunksStart + chunkOffset;
+            Vector3Int chunkPosition = GetChunkPosition(i);
+            Chunks[chunkPosition] = ReadChunkData(reader);
+        }
+
+        if(readToEnd)
+            reader.BaseStream.Position = chunksEnd;
+    }
+
+    protected virtual bool ReadChunk(BinaryReader reader, Vector3Int chunkPosition, bool readToEnd = true)
+    {
+        long[] chunkOffsets = ReadChunkOffsets(reader);
+        long chunksEnd = reader.ReadInt64();
+
+        int index = GetChunkIndex(chunkPosition);
+        var chunkOffset = chunkOffsets[index];
+        if(chunkOffset >= 0)
+        {
+            reader.BaseStream.Position += chunkOffset;
+            Chunks[chunkPosition] = ReadChunkData(reader);
+        }
+
+        if(readToEnd)
+            reader.BaseStream.Position = chunksEnd;
+
+        return true;
+    }
+
+    protected virtual long[] ReadChunkOffsets(BinaryReader reader)
+    {
+        long[] chunkOffsets = new long[MaxChunksCount];
+        for(int i = 0; i < MaxChunksCount; ++i)
+            chunkOffsets[i] = reader.ReadInt64();
+        return chunkOffsets;
+    }
+
+    protected abstract T ReadChunkData(BinaryReader reader);
+
+    #endregion
+
+    #region write
+
+    protected virtual void WriteAdditionalData(BinaryWriter writer) { }
+
+    public virtual void Write(BinaryWriter writer)
+    {
+        WriteAdditionalData(writer);
+        WriteChunks(writer);
+    }
+
+    protected virtual void WriteChunks(BinaryWriter writer)
+    {
+        long chunksOffsetsStart = writer.BaseStream.Position;
+
+        for(int i = 0; i < MaxChunksCount; ++i)
+            writer.Write(-1L);
+        writer.Write(-1L); // writing end chunks offset
+
+        long chunksStart = writer.BaseStream.Position;
+
+        foreach(var (chunkPosition, chunkData) in Chunks)
+        {
+            using(StreamPositionRememberer rememberer = writer)
+            {
+                int chunkIndex = GetChunkIndex(chunkPosition);
+                long chunkOffset = rememberer.StartPosition - chunksStart;
+                writer.BaseStream.Position = chunksOffsetsStart + chunkIndex * sizeof(long);
+                writer.Write(chunkOffset);
+            }
+            WriteChunkData(writer, chunkData);
+        }
+
+        using(StreamPositionRememberer rememberer = writer)
+        {
+            writer.BaseStream.Position = chunksOffsetsStart + MaxChunksCount * sizeof(long);
+            writer.Write(rememberer.StartPosition);
+        }
+    }
+
+    protected abstract void WriteChunkData(BinaryWriter writer, T data);
+
+    #endregion
+
+
+    protected virtual int GetChunkIndex(Vector3Int chunkPosition)
+    {
+        return chunkPosition.z + RegionSize.z * (chunkPosition.y + chunkPosition.x * RegionSize.y);
+    }
+
+    protected virtual Vector3Int GetChunkPosition(int chunkIndex)
+    {
+        int z = chunkIndex % RegionSize.z;
+        chunkIndex /= RegionSize.z;
+        int y = chunkIndex % RegionSize.y;
+        chunkIndex /= RegionSize.y;
+        int x = chunkIndex % RegionSize.x;
+        return new(x, y, z);
+    }
+}
