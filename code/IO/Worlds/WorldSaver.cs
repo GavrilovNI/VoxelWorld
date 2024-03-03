762d948d05c91f0889b79bf023219af2405d96e5
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-03 19:21:20 +0100

    feat: add WorldSaver.SaveBlocks

--- code/IO/Worlds/WorldSaver.cs
+++ code/IO/Worlds/WorldSaver.cs
@@ -4,6 +4,7 @@ using Sandcube.IO.Helpers;
 using Sandcube.Mth;
 using Sandcube.Worlds;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,16 +19,32 @@ public class WorldSaver : Component, ISaver
     protected WorldOptions WorldOptions => World.WorldOptions;
 
 
-    public virtual Task<bool> Save()
+    public virtual async Task<bool> Save()
     {
         if(WorldOptions.ChunkSize != World.ChunkSize)
             throw new InvalidOperationException($"Can't save world, saved chunk size {WorldOptions.ChunkSize} is not equal world's chunk size {World.ChunkSize}");
 
         SaveMarker saveMarker = SaveMarker.NewNotSaved;
-        var chunksData = World.Save(saveMarker);
-        var regions = chunksData.GroupBy(c => (1f * c.Key / WorldOptions.RegionSize).Floor());
+        var worldData = World.Save(saveMarker);
 
         var worldSaveHelper = new WorldSaveHelper(WorldFileSystem);
+
+        var blocksTask = SaveBlocks(worldSaveHelper, worldData.Chunks);
+
+        var results = await Task.WhenAll(blocksTask);
+
+        bool saved = results.All(saved => saved);
+        
+        if(saved)
+            saveMarker.MarkSaved();
+
+        return saved;
+    }
+
+    protected virtual Task<bool> SaveBlocks(WorldSaveHelper worldSaveHelper, IReadOnlyDictionary<Vector3Int, BlocksData> chunksData)
+    {
+        var regions = chunksData.GroupBy(c => (1f * c.Key / WorldOptions.RegionSize).Floor());
+
         var regionSaveHelper = new RegionSaveHelper(WorldOptions);
         foreach(var region in regions)
         {
@@ -52,8 +69,6 @@ public class WorldSaver : Component, ISaver
             using var writer = new BinaryWriter(regionWriteStream);
             regionSaveHelper.Write(writer);
         }
-
-        saveMarker.MarkSaved();
         return Task.FromResult(true);
     }
 }
