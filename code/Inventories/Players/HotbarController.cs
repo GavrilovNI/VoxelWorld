888b54cee8eba7611837fed4c2907322b8d45964
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-05 01:34:09 +0100

    feat: separate HotbarController to methods

--- code/Inventories/Players/HotbarController.cs
+++ code/Inventories/Players/HotbarController.cs
@@ -15,41 +15,50 @@ public class HotbarController : Component
 
     protected override void OnUpdate()
     {
-        if(!(MenuController.Instance?.IsAnyOpened ?? false))
-            UpdateHandSlot();
+        bool canUse = Inventory is not null && !(MenuController.Instance?.IsAnyOpened ?? false);
+        if(canUse)
+        {
+            ChangeMainHandIndex();
+
+            if(Input.Pressed("HandSwap"))
+                SwapHands();
+        }
     }
 
-    protected virtual void UpdateHandSlot()
+    protected virtual int ChangeMainHandIndex()
     {
-        if(Inventory is null)
-            return;
-
         var hotbar = Inventory.Hotbar;
-        if(hotbar.Size != 0)
-        {
-            for(int i = 0; i < hotbar.Size; ++i)
-            {
-                if(Input.Pressed($"Slot{i + 1}"))
-                    Inventory.MainHandIndex = i;
-            }
+        if(hotbar.Size == 0)
+            return Inventory.MainHandIndex;
 
-            int slotMoveDelta = (Input.Pressed("SlotPrev") ? -1 : 0) + (Input.Pressed("SlotNext") ? 1 : 0);
-            slotMoveDelta -= (int)Input.MouseWheel.y;
-            var newIndex = Inventory.MainHandIndex + slotMoveDelta;
-            Inventory.MainHandIndex = (newIndex % hotbar.Size + hotbar.Size) % hotbar.Size;
+        for(int i = 0; i < hotbar.Size; ++i)
+        {
+            if(Input.Pressed($"Slot{i + 1}"))
+                Inventory.MainHandIndex = i;
         }
 
-        if(Input.Pressed("HandSwap"))
-        {
-            var mainHandStack = Inventory.GetHandItem(HandType.Main);
-            var secondaryHandStack = Inventory.GetHandItem(HandType.Secondary);
+        int slotMoveDelta = (Input.Pressed("SlotPrev") ? -1 : 0) + (Input.Pressed("SlotNext") ? 1 : 0);
+        slotMoveDelta -= (int)Input.MouseWheel.y;
+        var newIndex = Inventory.MainHandIndex + slotMoveDelta;
+        newIndex = (newIndex % hotbar.Size + hotbar.Size) % hotbar.Size;
+        Inventory.MainHandIndex = newIndex;
+        return newIndex;
+    }
 
-            if(Inventory.TrySetHandItem(HandType.Main, secondaryHandStack, true) &&
-                Inventory.TrySetHandItem(HandType.Secondary, mainHandStack, true))
-            {
-                Inventory.TrySetHandItem(HandType.Main, secondaryHandStack);
-                Inventory.TrySetHandItem(HandType.Secondary, mainHandStack);
-            }
+    protected virtual bool SwapHands()
+    {
+        var mainHandStack = Inventory.GetHandItem(HandType.Main);
+        var secondaryHandStack = Inventory.GetHandItem(HandType.Secondary);
+
+        bool canSwap = Inventory.TrySetHandItem(HandType.Main, secondaryHandStack, true) &&
+            Inventory.TrySetHandItem(HandType.Secondary, mainHandStack, true);
+
+        if(canSwap)
+        {
+            Inventory.TrySetHandItem(HandType.Main, secondaryHandStack);
+            Inventory.TrySetHandItem(HandType.Secondary, mainHandStack);
         }
+
+        return canSwap;
     }
 }
