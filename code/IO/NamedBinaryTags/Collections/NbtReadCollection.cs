6edff151ffc49030b5f56052355b7a6079dabeb8
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-06 20:13:31 +0100

    feat: rename NbtReadCollection.TryGet => TryGetTag

--- code/IO/NamedBinaryTags/Collections/NbtReadCollection.cs
+++ code/IO/NamedBinaryTags/Collections/NbtReadCollection.cs
@@ -13,7 +13,7 @@ public abstract class NbtReadCollection<TKey> : BinaryTag
     public abstract bool HasTag(TKey key);
     public abstract BinaryTag GetTag(TKey key);
 
-    public bool TryGet(TKey key, out BinaryTag tag)
+    public bool TryGetTag(TKey key, out BinaryTag tag)
     {
         if(!HasTag(key))
         {
@@ -41,105 +41,105 @@ public abstract class NbtReadCollection<TKey> : BinaryTag
 
     public byte Get<T>(TKey key, byte defaultValue = default) where T : IEquatable<byte>
     {
-        if(TryGet(key, out var tag) && tag is ByteTag realTag)
+        if(TryGetTag(key, out var tag) && tag is ByteTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public sbyte Get<T>(TKey key, sbyte defaultValue = default) where T : IEquatable<sbyte>
     {
-        if(TryGet(key, out var tag) && tag is SByteTag realTag)
+        if(TryGetTag(key, out var tag) && tag is SByteTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public short Get<T>(TKey key, short defaultValue = default) where T : IEquatable<short>
     {
-        if(TryGet(key, out var tag) && tag is SByteTag realTag)
+        if(TryGetTag(key, out var tag) && tag is SByteTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public ushort Get<T>(TKey key, ushort defaultValue = default) where T : IEquatable<ushort>
     {
-        if(TryGet(key, out var tag) && tag is UShortTag realTag)
+        if(TryGetTag(key, out var tag) && tag is UShortTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public int Get<T>(TKey key, int defaultValue = default) where T : IEquatable<int>
     {
-        if(TryGet(key, out var tag) && tag is IntTag realTag)
+        if(TryGetTag(key, out var tag) && tag is IntTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public uint Get<T>(TKey key, uint defaultValue = default) where T : IEquatable<uint>
     {
-        if(TryGet(key, out var tag) && tag is UIntTag realTag)
+        if(TryGetTag(key, out var tag) && tag is UIntTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public long Get<T>(TKey key, long defaultValue = default) where T : IEquatable<long>
     {
-        if(TryGet(key, out var tag) && tag is LongTag realTag)
+        if(TryGetTag(key, out var tag) && tag is LongTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public ulong Get<T>(TKey key, ulong defaultValue = default) where T : IEquatable<ulong>
     {
-        if(TryGet(key, out var tag) && tag is ULongTag realTag)
+        if(TryGetTag(key, out var tag) && tag is ULongTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public float Get<T>(TKey key, float defaultValue = default) where T : IEquatable<float>
     {
-        if(TryGet(key, out var tag) && tag is FloatTag realTag)
+        if(TryGetTag(key, out var tag) && tag is FloatTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public double Get<T>(TKey key, double defaultValue = default) where T : IEquatable<double>
     {
-        if(TryGet(key, out var tag) && tag is DoubleTag realTag)
+        if(TryGetTag(key, out var tag) && tag is DoubleTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public decimal Get<T>(TKey key, decimal defaultValue = default) where T : IEquatable<decimal>
     {
-        if(TryGet(key, out var tag) && tag is DecimalTag realTag)
+        if(TryGetTag(key, out var tag) && tag is DecimalTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public bool Get<T>(TKey key, bool defaultValue = default) where T : IEquatable<bool>
     {
-        if(TryGet(key, out var tag) && tag is BoolTag realTag)
+        if(TryGetTag(key, out var tag) && tag is BoolTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public char Get<T>(TKey key, char defaultValue = default) where T : IEquatable<char>
     {
-        if(TryGet(key, out var tag) && tag is CharTag realTag)
+        if(TryGetTag(key, out var tag) && tag is CharTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public string Get<T>(TKey key, string defaultValue = "") where T : IEquatable<string>
     {
-        if(TryGet(key, out var tag) && tag is StringTag realTag)
+        if(TryGetTag(key, out var tag) && tag is StringTag realTag)
             return realTag.Value;
         return defaultValue;
     }
 
     public T Get<T>(TKey key, T defaultValue) where T : struct, Enum
     {
-        if(TryGet(key, out var tag) && tag is IntTag intTag)
+        if(TryGetTag(key, out var tag) && tag is IntTag intTag)
             return Enum.GetValues<T>()[intTag.Value];
         return defaultValue;
     }
@@ -160,7 +160,7 @@ public abstract class NbtReadCollection<TKey> : BinaryTag
 
     public bool TryReadBy<T>(TKey key, ref T value) where T : INbtReadable
     {
-        if(!TryGet(key, out var tag))
+        if(!TryGetTag(key, out var tag))
             return false;
         value.Read(tag);
         return true;
