ca87187ff898003b8c68d83c8e24af8f10d6a327
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-11 16:03:37 +0100

    feat: add MultiValueBiDictionary

---
+++ code/Data/IReadOnlyMultiValueBiDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Sandcube.Data;
+
+public interface IReadOnlyMultiValueBiDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>> where TKey : notnull where TValue : notnull
+{
+    int ValuesCount { get; }
+    int KeysCount { get; }
+
+    IEnumerable<TKey> Keys { get; }
+    IEnumerable<TValue> Values { get; }
+
+    TKey GetKey(TValue value);
+    TKey GetKeyOrDefault(TValue value, TKey defaultKey);
+
+    IReadOnlySet<TValue> GetValues(TKey key);
+    IReadOnlySet<TValue> GetValuesOrDefault(TKey key, IReadOnlySet<TValue> defaultValues);
+
+    bool TryGetKey(TValue value, out TKey key);
+    bool TryGetValues(TKey key, out IReadOnlySet<TValue> values);
+
+    bool ContainsKey(TKey key);
+    bool ContainsValue(TValue value);
+
+    public IReadOnlySet<TValue> this[TKey key] { get; }
+    public TKey this[TValue key] { get; }
+}
+
+public static class IReadOnlyMultiValueBiDictionaryExtensions
+{
+    public static IReadOnlySet<TValue> GetValuesOrEmpty<TKey, TValue>(this IReadOnlyMultiValueBiDictionary<TKey, TValue> dictionary, TKey key) where TKey : notnull where TValue : notnull =>
+        dictionary.GetValuesOrDefault(key, new HashSet<TValue>());
+}
