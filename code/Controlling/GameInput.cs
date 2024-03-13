71b6c25baebf4ba05d58c27750a540021eb4d82e
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-13 05:30:15 +0100

    feat: add GameInput

---
+++ code/Controlling/GameInput.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+namespace Sandcube.Controlling;
+
+public static class GameInput
+{
+    public static Vector2 LookDelta => Input.MouseDelta * Preferences.Sensitivity;
+
+    public static bool IsCrouching => Input.Down("Duck");
+    public static bool IsRunning => Input.Down("Run");
+    public static bool IsJumpPressed => Input.Pressed("Jump");
+}
