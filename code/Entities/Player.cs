b6dd251d1fdf3ac6b16c9513a003b8df54e5ba08
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-13 05:33:40 +0100

    fix: save player's look direction correctly

--- code/Entities/Player.cs
+++ code/Entities/Player.cs
@@ -15,6 +15,7 @@ namespace Sandcube.Entities;
 public class Player : Entity
 {
     [Property] public GameObject Eye { get; private set; } = null!;
+    [Property] public CameraComponent Camera { get; private set; } = null!;
     [Property] public ItemDropper ItemDropper { get; private set; } = null!;
     [Property] public bool IsCreative { get; private set; } = false;
     [Property] public float ReachDistance { get; private set; } = 39.37f * 5;
@@ -58,7 +59,8 @@ public class Player : Entity
     protected override BinaryTag WriteAdditional()
     {
         CompoundTag tag = new();
-        tag.Set("eye", Eye.Transform.Local);
+        tag.Set("eye_rotation", Eye.Transform.LocalRotation);
+        tag.Set("camera_rotation", Camera.Transform.LocalRotation);
         tag.Set("creative", IsCreative);
         tag.Set("reach_distance", ReachDistance);
         tag.Set("invetory", Inventory);
@@ -68,7 +70,8 @@ public class Player : Entity
     protected override void ReadAdditional(BinaryTag tag)
     {
         CompoundTag compoundTag = tag.To<CompoundTag>();
-        Eye.Transform.Local = compoundTag.Get<Transform>("eye");
+        Eye.Transform.LocalRotation = compoundTag.Get<Rotation>("eye_rotation");
+        Camera.Transform.LocalRotation = compoundTag.Get<Rotation>("camera_rotation");
         IsCreative = compoundTag.Get<bool>("creative");
         ReachDistance = compoundTag.Get<float>("reach_distance");
         Inventory.Read(compoundTag.GetTag("invetory"));
