c19d992a8e119ea22dbbbf283f3a475e0f514e3e
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-16 00:00:41 +0100

    feat: put hotbar inputs to GameInput

--- code/Controlling/GameInput.cs
+++ code/Controlling/GameInput.cs
@@ -9,4 +9,10 @@ public static class GameInput
     public static bool IsCrouching => Input.Down("Duck");
     public static bool IsRunning => Input.Down("Run");
     public static bool IsJumpPressed => Input.Pressed("Jump");
+    public static bool IsHandSwapPressed => Input.Pressed("HandSwap");
+    public static bool IsDropPressed => Input.Pressed("Drop");
+
+    public static bool IsSlotPressed(int slotIndex) => Input.Pressed($"Slot{slotIndex + 1}");
+    public static bool IsSlotPrevPressed => Input.Pressed("SlotPrev");
+    public static bool IsSlotNextPressed => Input.Pressed("SlotNext");
 }
