1d0c1c21c0493ab78fdd410069f92b2c3740f182
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-18 00:27:51 +0100

    feat: add LocalPlayerCameraDuplicate

---
+++ code/Players/LocalPlayerCameraDuplicate.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using System.Linq;
+using VoxelWorld.Entities;
+using VoxelWorld.SandcubeExtensions;
+
+namespace VoxelWorld.Players;
+
+public class LocalPlayerCameraDuplicate : Component, ILocalPlayerInitializable
+{
+    [Property, RequireComponent] protected CameraComponent Camera { get; set; } = null!;
+    [Property] protected CameraComponent? PlayerCamera { get; set; }
+
+    public void InitializeLocalPlayer(Player player)
+    {
+        PlayerCamera = player.Camera;
+        PlayerCamera.CopyPropertiesTo(Camera);
+    }
+
+    protected override void OnAwake() => PlayerCamera?.CopyPropertiesTo(Camera);
+
+    protected override void OnUpdate()
+    {
+        if(PlayerCamera is not null)
+            Transform.World = PlayerCamera.Transform.World;
+        Camera.Enabled = PlayerCamera is not null && !PlayerCamera.Active;
+    }
+}
---
+++ code/SandcubeExtensions/ComponentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace VoxelWorld.SandcubeExtensions;
+
+public static class ComponentExtensions
+{
+    public static void CopyPropertiesTo(this Component source, Component destination)
+    {
+        var sourceCameraType = TypeLibrary.GetType(source.GetType());
+        var destinationCameraType = TypeLibrary.GetType(destination.GetType());
+
+        var sourceCameraProperties = sourceCameraType.Properties.Where(p => p.Attributes.Any(a => a is PropertyAttribute));
+        var destinationCameraProperties = destinationCameraType.Properties.Where(p => p.Attributes.Any(a => a is PropertyAttribute));
+
+        var propertiesToCopy = sourceCameraProperties.IntersectBy(destinationCameraProperties, p => p);
+
+        foreach(var property in propertiesToCopy)
+            property.SetValue(destination, property.GetValue(source));
+    }
+}
