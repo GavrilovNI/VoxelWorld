bd388986b67ac51210085f8097bca11c2bbb0977
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-22 22:17:28 +0100

    feat: add FakeFootStepSoundPlayer

--- code/Blocks/Properties/BlockProperties.cs
+++ code/Blocks/Properties/BlockProperties.cs
@@ -9,6 +9,7 @@ public readonly record struct BlockProperties
     public bool IsTransparent { get; init; } = false;
     public string PlaceSound { get; init; } = "sounds/voxelworld/blocks/block_place.sound";
     public string BreakSound { get; init; } = "sounds/voxelworld/blocks/block_break.sound";
+    public string FootstepSound { get; init; } = "sounds/voxelworld/blocks/block_footstep.sound";
 
     public BlockProperties()
     {
