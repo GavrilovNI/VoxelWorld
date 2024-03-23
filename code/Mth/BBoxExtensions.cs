9f4d28c67097a7502932c70282f38b876ac51bb5
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-23 17:44:22 +0100

    feat: Add BBox.AlmostEqual(BBox), Remove BBox.AlmostEqual(BBoxInt)

--- code/Mth/BBoxExtensions.cs
+++ code/Mth/BBoxExtensions.cs
@@ -4,7 +4,8 @@ namespace VoxelWorld.Mth;
 
 public static class BBoxExtensions
 {
-    public static bool AlmostEqual(this BBox @this, in BBoxInt other, float delta = 0.0001f) => other.AlmostEqual(@this, delta);
+    public static bool AlmostEqual(this BBox @this, in BBox other, float delta = 0.0001f) =>
+        @this.Mins.AlmostEqual(other.Mins, delta) && @this.Maxs.AlmostEqual(other.Maxs, delta);
 
     public static BBoxInt Floor(this BBox bbox) => new(bbox.Mins.Floor(), bbox.Maxs.Floor());
     public static BBoxInt Round(this BBox bbox) => new(bbox.Mins.Round(), bbox.Maxs.Round());
