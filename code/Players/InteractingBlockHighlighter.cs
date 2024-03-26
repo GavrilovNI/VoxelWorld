93dc8de5757c0006e721fb7305309ef601cc9a1e
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-03-26 08:41:55 +0100

    feat: add block highlighting

---
+++ code/Players/InteractingBlockHighlighter.cs
@@ -0,0 +1,57 @@
+﻿using Sandbox;
+using System;
+using VoxelWorld.Rendering;
+using VoxelWorld.Worlds;
+
+namespace VoxelWorld.Players;
+
+public class InteractingBlockHighlighter : Component
+{
+    [Property, RequireComponent] protected WorldInteractor WorldInteractor { get; set; } = null!;
+    [Property, RequireComponent] protected GameObject HighlightedBlockPrefab { get; set; } = null!;
+
+    protected HighlightedBlock HighlightedBlock = null!;
+
+
+    protected virtual void UpdateHighlightedBlock()
+    {
+        if(WorldInteractor.Active)
+            UpdateHighlightedBlock(WorldInteractor.TraceResult);
+        else
+            HighlightedBlock.GameObject.Enabled = false;
+    }
+
+    protected virtual void UpdateHighlightedBlock(PhysicsTraceResult traceResult)
+    {
+        if(!traceResult.Hit || !World.TryFindInObject(traceResult.Body?.GetGameObject(), out var world))
+        {
+            HighlightedBlock.GameObject.Enabled = false;
+            return;
+        }
+
+        var blockPosition = world.GetBlockPosition(traceResult.EndPosition, traceResult.Normal);
+        HighlightedBlock.Transform.World = world.GameObject.Transform.World.WithPosition(world.GetBlockGlobalPosition(blockPosition));
+        HighlightedBlock.BlockState = world.GetBlockState(blockPosition);
+        HighlightedBlock.GameObject.Enabled = true;
+    }
+
+
+    protected override void OnAwake()
+    {
+        var gameObject = HighlightedBlockPrefab.Clone();
+        HighlightedBlock = gameObject.Components.Get<HighlightedBlock>();
+        if(!HighlightedBlock.IsValid())
+            throw new InvalidOperationException($"Couldn't create {nameof(Rendering.HighlightedBlock)}");
+
+        UpdateHighlightedBlock();
+    }
+
+    protected override void OnUpdate() => UpdateHighlightedBlock();
+    protected override void OnEnabled()
+    {
+        HighlightedBlock.GameObject.Enabled = true;
+        UpdateHighlightedBlock();
+    }
+    protected override void OnDisabled() => HighlightedBlock.GameObject.Enabled = false;
+    protected override void OnDestroy() => HighlightedBlock.GameObject.Destroy();
+}
---
+++ code/Rendering/HighlightedBlock.cs
@@ -0,0 +1,53 @@
+﻿using Sandbox;
+using VoxelWorld.Blocks.States;
+using VoxelWorld.Meshing;
+
+namespace VoxelWorld.Rendering;
+
+public class HighlightedBlock : Component
+{
+    [Property, RequireComponent] protected UnlimitedModelRenderer ModelRenderer { get; set; } = null!;
+    [Property] protected Material Material { get; set; } = null!;
+
+    private BlockState? _blockState;
+    public BlockState BlockState
+    {
+        get => _blockState ?? BlockState.Air;
+        set
+        {
+            if (BlockState == value)
+                return;
+
+            _blockState = value;
+            if(Active)
+                UpdateRenderer();
+        }
+    }
+
+
+    public virtual void UpdateRenderer()
+    {
+        if(BlockState.IsAir())
+        {
+            ModelRenderer.Enabled = false;
+            return;
+        }
+
+        var modelMeshBuilder = new UnlimitedMesh<Vector3Vertex>.Builder()
+            .Add(GameController.Instance!.BlockMeshes.GetInteraction(BlockState)!);
+        //modelMeshBuilder = modelMeshBuilder.Scale(modelMeshBuilder.Bounds.Center, 1f + 1f / modelMeshBuilder.Bounds.Size);
+
+        var modelMesh = modelMeshBuilder.Build();
+
+        ModelRenderer.SetModels(modelMesh, Material);
+        ModelRenderer.Enabled = Active;
+    }
+
+    protected override void OnEnabled()
+    {
+        UpdateRenderer();
+        ModelRenderer.Enabled = true;
+    }
+
+    protected override void OnDisabled() => ModelRenderer.Enabled = false;
+}
