1d30b49e99a333838eef9cbada69143e0b5d2f4f
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-04-06 20:05:36 +0200

    feat: add GizmoDraw.DrawBboxTraceResult extensions

---
+++ code/Rendering/GizmoExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using static Sandbox.Gizmo;
+
+namespace VoxelWorld.Rendering;
+
+public static class GizmoExtensions
+{
+    public static void DrawBboxTraceResult(this GizmoDraw draw, SceneTraceResult traceResult, BBox bounds, Color? endColor = null, float startBackOffset = 0f)
+    {
+        draw.LineBBox(bounds + traceResult.StartPosition + -traceResult.Direction * startBackOffset);
+        draw.Line(traceResult.StartPosition - traceResult.Direction * startBackOffset, traceResult.EndPosition);
+        draw.Line(traceResult.StartPosition, traceResult.StartPosition + traceResult.Normal * 100f);
+        if(endColor.HasValue)
+            draw.Color = endColor.Value;
+        draw.LineBBox(bounds + traceResult.EndPosition);
+    }
+
+    public static void DrawBboxTraceResult(this GizmoDraw draw, PhysicsTraceResult traceResult, BBox bounds, Color? endColor = null, float startBackOffset = 0f)
+    {
+        draw.LineBBox(bounds + traceResult.StartPosition + -traceResult.Direction * startBackOffset);
+        draw.Line(traceResult.StartPosition - traceResult.Direction * startBackOffset, traceResult.EndPosition);
+        draw.Line(traceResult.StartPosition, traceResult.StartPosition + traceResult.Normal * 100f);
+        if(endColor.HasValue)
+            draw.Color = endColor.Value;
+        draw.LineBBox(bounds + traceResult.EndPosition);
+    }
+}
