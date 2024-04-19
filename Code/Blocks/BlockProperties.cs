b15c6bee2d7eeaf1b9ca4796043ebc4f27dce581
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-04-19 15:29:25 +0200

    feat: add BlockProperties.DamageSound, BlockProperties.IsBreakable & BlockProperties.Health, add block_damage.sound

--- Code/Blocks/BlockProperties.cs
+++ Code/Blocks/BlockProperties.cs
@@ -8,8 +8,13 @@ public readonly record struct BlockProperties
 
     public bool IsTransparent { get; init; } = false;
     public string PlaceSound { get; init; } = "sounds/voxelworld/blocks/block_place.sound";
+    public string DamageSound { get; init; } = "sounds/voxelworld/blocks/block_damage.sound";
     public string BreakSound { get; init; } = "sounds/voxelworld/blocks/block_break.sound";
     public string FootstepSound { get; init; } = "sounds/voxelworld/blocks/block_footstep.sound";
+
+    public bool IsBreakable { get; init; } = true;
+    public float Health { get; init; } = 100f;
+
 
     public BlockProperties()
     {
