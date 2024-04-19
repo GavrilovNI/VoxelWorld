88b62172ca9ddeef2e8f1c0447d1288e2a1aebd6
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-04-19 15:31:26 +0200

    feat: add EntityDamageInfo

---
+++ Code/Interactions/EntityDamageInfo.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+using VoxelWorld.Entities;
+using VoxelWorld.Inventories;
+using VoxelWorld.Items;
+
+namespace VoxelWorld.Interactions;
+
+public class EntityDamageInfo : DamageInfo
+{
+    public new Entity Attacker { get; set; }
+    public new Stack<Item> Weapon { get; set; }
+
+    public EntityDamageInfo(float damage, Entity attacker, Stack<Item> weapon) : base(damage, attacker.GameObject, null)
+    {
+        Attacker = attacker;
+        Weapon = weapon;
+    }
+}
