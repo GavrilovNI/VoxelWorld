134f78622f9da871bd7968d2282fce237f1533f8
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-04-19 15:18:51 +0200

    feat: add HandType.GetOpposite()

--- code/Interactions/HandType.cs
+++ code/Interactions/HandType.cs
@@ -7,3 +7,9 @@ public enum HandType
     Main,
     Secondary
 }
+
+public static class HandTypeExtensions
+{
+    public static HandType GetOpposite(this HandType hand) =>
+        hand == HandType.Main ? HandType.Secondary : HandType.Main;
+}
