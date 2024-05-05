2d4c8a23a80af1124b602be77e33bf18ecff69be
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-05-05 17:24:07 +0200

    fix: use Vector2IntB & Vector3IntB for VectorIntControlWidget

--- Editor/VectorIntControlWidget.cs
+++ Editor/VectorIntControlWidget.cs
@@ -1,10 +1,11 @@
 ﻿using Editor;
 using Sandbox;
+using VoxelWorld.Mth;
 
 namespace VoxelWorld.Editor;
 
-[CustomEditor(typeof(Vector3Int))]
-[CustomEditor(typeof(Vector2Int))]
+[CustomEditor(typeof(Vector3IntB))]
+[CustomEditor(typeof(Vector2IntB))]
 public class VectorIntControlWidget : VectorControlWidget
 {
     public VectorIntControlWidget(SerializedProperty property) : base(property)
