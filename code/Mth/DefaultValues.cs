04653434f2f3882aa0999a864c64fe5e1810458c
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-05-08 17:32:17 +0200

    feat: return RegionSize default value to 4x4x4

--- code/Mth/DefaultValues.cs
+++ code/Mth/DefaultValues.cs
@@ -9,7 +9,7 @@ public static class DefaultValues
     public const float DoorWidth = MathV.UnitsInMeter * 3 / 16;
 
     public static readonly Vector3Byte ChunkSize = new(16);
-    public static readonly Vector3Byte RegionSize = new(1);
+    public static readonly Vector3Byte RegionSize = new(4);
 
     public const float FlatItemPixelSize = MathV.UnitsInMeter / 16f;
     public const float FlatItemThickness = MathV.UnitsInMeter / 16f;
